b7c4bd0f4ddff1aff994f0d49addc9f78f86da08
Donny <doniwahyu14@gmail.com>
2023-04-05 14:07:51 +0700

    creaate sale order pending and approve

--- app/Http/Controllers/saleOrderController.php
+++ app/Http/Controllers/saleOrderController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SaleOrder;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
 
 class saleOrderController extends Controller
 {
@@ -11,7 +15,11 @@ class saleOrderController extends Controller
      */
     public function index()
     {
-        //
+        $sale = SaleOrder::latest()->paginate(10);
+        return [
+            "status" => "Success get all sale order",
+            "data" => $sale
+        ];
     }
 
     /**
@@ -27,7 +35,47 @@ class saleOrderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'Id_Customer' => 'required',
+            'Id_Product' => 'required',
+            'Quantity' => 'required',
+            'Unit_Price' => 'required',
+        ]);
+
+        $product = Product::where('id', $request->Id_Product)->get();
+        $user = Product::where('id', $request->Id_Customer)->get();
+        // if (count($product) > 0) {
+        //     Log::alert(true);
+        // } else {
+        //     Log::alert(false);
+        // }
+        if (count($user) < 1) {
+            return [
+                "code" => 404,
+                "status" => "Id customer doesn't exist",
+                "data" => null
+            ];
+        }
+        if (count($product) < 1) {
+            return [
+                "code" => 404,
+                "status" => "Id product doesn't exist",
+                "data" => null
+            ];
+        }
+
+        $sale = SaleOrder::create([
+            'id_customer' => $request->Id_Customer,
+            'id_product' => $request->Id_Product,
+            'quantity' => $request->Quantity,
+            'unit_price' => $request->Unit_Price,
+        ]);
+
+        return [
+            "code" => 200,
+            "status" => "Success add stock product",
+            "data" => $sale
+        ];
     }
 
     /**
@@ -51,7 +99,21 @@ class saleOrderController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $saleItem = SaleOrder::where('id', $id)->get();
+        if (count($saleItem) < 1) {
+            return [
+                "code" => 404,
+                "status" => "Id sale order doesn't exist",
+                "data" => null
+            ];
+        }
+        SaleOrder::where('id', $id)->update(['status' => 1]);
+        $sale = SaleOrder::where('id', $id)->get();
+        return [
+            "code" => 200,
+            "status" => "Approve success",
+            "data" => $sale
+        ];
     }
 
     /**
